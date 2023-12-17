5d4ea7859bb961ce83b6ba953ccae1d5cce0d501
Aggrey Kiprop <aggreykiprop60@gmail.com>
2023-12-17 13:21:00 +0300

    Create python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,37 @@
+#declaring two different variables
+x = "james"
+y = "Aggrey"
+
+print(x)
+print(y)
+
+#declaring a variable twice to change the type
+
+x = 2
+x = "James"
+
+print (x)
+
+
+#casting to specify the data type of a variable
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print(x)
+print(y)
+print(z)
+
+
+#getting a data type of a variable using function type()
+
+x = 5
+y = "James"
+
+print(type(x))
+print(type(y))
+
+
+#variables are case sensitive in python
+#strings can be declared by both single and double quotes
